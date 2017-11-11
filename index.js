dfebfba9d95b3278bffcb81ff619dcd941b872d8
Heverson Ribeiro Resende <hresende.net@gmail.com>
2017-11-11 16:03:02 -0200

    fix #1

--- index.js
+++ index.js
@@ -5,58 +5,49 @@ const fs = require('fs')
 const readline = require('readline')
 const chalk = require('chalk')
 const names = require('random-tree-names')
+const dirTemplates = `${__dirname}/templates`;
 
-// TODO: refactor: avoid duplicate
-let editorconfigContent
-let packageJSONContent
-
-fs.readFile(`${__dirname}/templates/editorconfig-template.toml`, 'utf8', (error, data) => {
-	if (error) throw error
-	editorconfigContent = data
-})
-
-fs.readFile(`${__dirname}/templates/packagejson-template.json`, 'utf8', (error, data) => {
-	if (error) throw error
-	packageJSONContent = data
-})
+let editorconfigContent = readFile(`${dirTemplates}/editorconfig-template.toml`);
+let packageJSONContent = readFile(`${dirTemplates}/packagejson-template.json`);
 
 const successLog = (text) => console.log(`${chalk.green("✔︎")} ${text}`)
 
+function readFile(dirfile){
+	fs.readFile(dirfile, 'utf8', (error, data) => {
+		if (error) throw error
+		return data;
+	})
+}
+
+function createFolder(folder){
+    fs.mkdir(projectName, (error) => {
+        if (error) throw error
+        successLog(`${folder} folder created!`)
+    })
+}
+
+function createFile(folder, file, content){
+    fs.writeFile(`${folder}/${file}`, `${content}`, (error) => {
+        if (error) throw error
+        successLog(`${folder}/${file} created!`)
+    })
+}
+
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 })
 
 let projectName = names.random('de')
 
-rl.question(`Project name (${projectName}): `,  (answer) => {
-	projectName = answer || projectName
+rl.question(`Project name (${projectName}): `, (answer) => {
+    projectName = answer || projectName
 
-	fs.mkdir(projectName, (error) => {
-		if (error) throw error
-		successLog(`${projectName} folder created!`)
-	})
+    createFolder(projectName);
+    createFile(projectName, '.npmrc', 'package-lock=false');
+    createFile(projectName, '.gitignore', 'node_modules');
+    createFile(projectName, '.editorconfig', editorconfigContent);
+    createFile(projectName, '.package.json', packageJSONContent);
 
-	// TODO: refactor: avoid duplicate
-	fs.writeFile(`${projectName}/.npmrc`, 'package-lock=false', (error) => {
-		if (error) throw error
-		successLog(`${projectName}/.npmrc created!`)
-	})
-
-	fs.writeFile(`${projectName}/.gitignore`, 'node_modules', (error) => {
-		if (error) throw error
-		successLog(`${projectName}/.gitiginore created!`)
-	})
-
-	fs.writeFile(`${projectName}/.editorconfig`, editorconfigContent, (error) => {
-		if (error) throw error
-		successLog(`${projectName}/.editorconfig created!`)
-	})
-
-	fs.writeFile(`${projectName}/package.json`, packageJSONContent, (error) => {
-		if (error) throw error
-		successLog(`${projectName}/package.json created!`)
-	})
-
-	rl.close()
+    rl.close()
 })
