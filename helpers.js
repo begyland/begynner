d0bbffeabc6a2f6da0bf7d869d80021be56e862d
Felipe Gilmar <felipe.gilmar@vipbrtelecom.com>
2023-01-27 19:42:45 -0300

    Removing semicolon

--- helpers.js
+++ helpers.js
@@ -2,57 +2,57 @@ const fs = require('fs').promises
 const chalk = require('chalk')
 const makeDir = require('make-dir')
 const Mustache = require('mustache')
-const path = require('path');
+const path = require('path')
 
 exports.successLog = (text) => console.log(`${chalk.green("✅")} ${text}`)
 exports.errorLog = (text) => console.log(`${chalk.red("❌")} ${text}`)
 exports.infoLog = (text) => console.log(`\n\n${chalk.green(`${text}`)} `)
 
 exports.rreaddir = async (filePath) => {
-  const dir = await fs.readdir(filePath);
+  const dir = await fs.readdir(filePath)
   const files = await Promise.all(dir.map(async relativePath => {
-    const absolutePath = path.join(filePath, relativePath);
-    const stat = await fs.lstat(absolutePath);
+    const absolutePath = path.join(filePath, relativePath)
+    const stat = await fs.lstat(absolutePath)
 
-    return stat.isDirectory() ? rreaddir(absolutePath) : absolutePath;
-  }));
+    return stat.isDirectory() ? rreaddir(absolutePath) : absolutePath
+  }))
 
-  return files.flat();
+  return files.flat()
 }
 
 exports.makeProject = async (data) => {
 
   try {
 
-    let dirTemplate = path.resolve(__dirname, "templates");
+    let dirTemplate = path.resolve(__dirname, "templates")
 
-    const projectFolder = data.projectname;
+    const projectFolder = data.projectname
     const githubFolder = `${projectFolder}/.github`
 
-    let templateExist = this.rreaddir(dirTemplate + "/" + data.template);
+    let templateExist = this.rreaddir(dirTemplate + "/" + data.template)
 
     templateExist.then(async (fileDir) => {
 
-      await this.createFolder(projectFolder);
-      await this.createFolder(githubFolder);
+      await this.createFolder(projectFolder)
+      await this.createFolder(githubFolder)
 
       await Promise.all(fileDir.map(async relativePath => {
-        let nameFile = relativePath.split("\\").pop();
-        let renderMustache = ["readme.md", "package.json", "license.md"].find(x => x == nameFile);
-        const readFile = renderMustache ? Mustache.render(await fs.readFile(relativePath, 'utf8'), data) : await fs.readFile(relativePath, 'utf8');
-        await this.createFile(projectFolder, nameFile, readFile);
-      }));
+        let nameFile = relativePath.split("\\").pop()
+        let renderMustache = ["readme.md", "package.json", "license.md"].find(x => x == nameFile)
+        const readFile = renderMustache ? Mustache.render(await fs.readFile(relativePath, 'utf8'), data) : await fs.readFile(relativePath, 'utf8')
+        await this.createFile(projectFolder, nameFile, readFile)
+      }))
 
       this.infoLog(`Parabéns projeto ${data.projectname} criado com sucesso!`)
 
     }).catch((err) => {
-      this.errorLog("Template not found");
+      this.errorLog("Template not found")
     })
 
-    return (await templateExist).length;
+    return (await templateExist).length
 
   } catch (err) {
-    this.errorLog(err);
+    this.errorLog(err)
   }
 }
 
