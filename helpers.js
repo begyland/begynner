ee93d2d5beacfdaae01d190a2328c2497d170e41
Felipe Gilmar <felipe.gilmar@vipbrtelecom.com>
2023-01-27 21:02:48 -0300

    Simplified structure and template choice option

--- helpers.js
+++ helpers.js
@@ -4,64 +4,75 @@ const makeDir = require('make-dir')
 const Mustache = require('mustache')
 const path = require('path')
 
-exports.successLog = (text) => console.log(`${chalk.green("✅")} ${text}`)
-exports.errorLog = (text) => console.log(`${chalk.red("❌")} ${text}`)
+exports.successLog = (text) => console.log(`${chalk.green('✅')} ${text}`)
+exports.errorLog = (text) => console.log(`${chalk.red('❌')} ${text}`)
 exports.infoLog = (text) => console.log(`\n\n${chalk.green(`${text}`)} `)
 
 exports.rreaddir = async (filePath) => {
   const dir = await fs.readdir(filePath)
-  const files = await Promise.all(dir.map(async relativePath => {
-    const absolutePath = path.join(filePath, relativePath)
-    const stat = await fs.lstat(absolutePath)
+  const files = await Promise.all(
+    dir.map(async (relativePath) => {
+      const absolutePath = path.join(filePath, relativePath)
+      const stat = await fs.lstat(absolutePath)
 
-    return stat.isDirectory() ? rreaddir(absolutePath) : absolutePath
-  }))
+      return stat.isDirectory() ? rreaddir(absolutePath) : absolutePath
+    })
+  )
 
   return files.flat()
 }
 
 exports.makeProject = async (data) => {
-
   try {
+    let dirTemplateModel = path.resolve(__dirname, 'templates/_model')
+    let dirTemplate = path.resolve(__dirname, 'templates')
+    const { projectname, template } = data
 
-    let dirTemplate = path.resolve(__dirname, "templates")
+    const projectFolder = projectname
 
-    const projectFolder = data.projectname
-    const githubFolder = `${projectFolder}/.github`
+    let filePath = []
+    let templatePath = await this.rreaddir(dirTemplate + '/' + template)
+    let modelPath = await this.rreaddir(dirTemplateModel)
 
-    let templateExist = this.rreaddir(dirTemplate + "/" + data.template)
+    filePath = [...templatePath, ...modelPath]
 
-    templateExist.then(async (fileDir) => {
+    if (!filePath) {
+      this.errorLog('Template not found')
+    }
 
-      await this.createFolder(projectFolder)
-      await this.createFolder(githubFolder)
+    await this.createFolder(projectFolder)
+    await this.createFolder(`${projectFolder}/.github`)
 
-      await Promise.all(fileDir.map(async relativePath => {
-        let nameFile = relativePath.split("\\").pop()
-        let renderMustache = ["readme.md", "package.json", "license.md"].find(x => x == nameFile)
-        const readFile = renderMustache ? Mustache.render(await fs.readFile(relativePath, 'utf8'), data) : await fs.readFile(relativePath, 'utf8')
+    await Promise.all(
+      filePath.map(async (relativePath) => {
+        let nameFile = relativePath.split('/').pop()
+        let renderMustache = ['readme.md', 'package.json', 'license.md'].find(
+          (x) => x == nameFile
+        )
+        const readFile = renderMustache
+          ? Mustache.render(await fs.readFile(relativePath, 'utf8'), data)
+          : await fs.readFile(relativePath, 'utf8')
+
         await this.createFile(projectFolder, nameFile, readFile)
-      }))
+      })
+    )
 
-      this.infoLog(`Parabéns projeto ${data.projectname} criado com sucesso!`)
+    this.infoLog(
+      `Congratulations project ${projectFolder} successfully created!`
+    )
 
-    }).catch((err) => {
-      this.errorLog("Template not found")
-    })
-
-    return (await templateExist).length
-
+    return true
   } catch (err) {
     this.errorLog(err)
   }
 }
 
 exports.createFolder = async (folder) => {
-  await makeDir(folder)
-    .then((path) => this.successLog(`${path} created!`))
+  await makeDir(folder).then((path) => this.successLog(`${path} created!`))
 }
 
 exports.createFile = async (folder, file, content) => {
-  await fs.writeFile(`${folder}/${file}`, content)
+  await fs
+    .writeFile(`${folder}/${file}`, content)
     .then(() => this.successLog(`${folder}/${file} created!`))
 }
--- index.js
+++ index.js
@@ -4,25 +4,28 @@
 const helpers = require('./helpers')
 const readline = require('readline')
 const names = require('random-tree-names')
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
 
-rl.question('GitHub username: ', async (usernameAnswer) => {
-  let projectName = await names.random('de');
+let templates = [{ name: 'simple' }, { name: 'api-simple' }]
+
+rl.question('GitHub Username: ', async (usernameAnswer) => {
+  let projectName = await names.random('de')
   rl.question(`Project name (${projectName}): `, async (projectNameAnswer) => {
-    rl.question(`choose template: `, async (modelAnswer) => {
-
+    console.table(templates)
+    rl.question(`choose template:`, async (modelAnswer) => {
       const data = {
-        projectname: projectNameAnswer || projectName || "simple_project",
-        username: usernameAnswer || 'username',
-        year: (new Date().getFullYear()),
-        template: modelAnswer || "simple"
+        projectname: projectNameAnswer || projectName,
+        username: usernameAnswer || process.env.USER,
+        year: new Date().getFullYear(),
+        template: templates[modelAnswer].name,
       }
 
-      helpers.makeProject(data);
+      helpers.makeProject(data)
 
-      rl.close();
+      rl.close()
     })
   })
 })
-
-
