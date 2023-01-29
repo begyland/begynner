92b8e28f6ed5d3fdb295c5293dfcda60d70080bc
Marcos Garcia de Lima <marckfree@gmail.com>
2023-01-29 17:29:51 -0300

    Fixes

--- tests/rreaddir.js
+++ tests/rreaddir.js
@@ -1,17 +1,19 @@
-const fs = require('fs').promises;
-const path = require('path');
+const fs = require('fs').promises
+const path = require('path')
 
 async function rreaddir(filePath) {
-  const dir = await fs.readdir(filePath);
-  const files = await Promise.all(dir.map(async relativePath => {
-    const absolutePath = path.join(filePath, relativePath);
-    const stat = await fs.lstat(absolutePath);
+  const dir = await fs.readdir(filePath)
+  const files = await Promise.all(
+    dir.map(async relativePath => {
+      const absolutePath = path.join(filePath, relativePath)
+      const stat = await fs.lstat(absolutePath)
 
-    return stat.isDirectory() ? rreaddir(absolutePath) : absolutePath;
-  }));
+      return stat.isDirectory() ? rreaddir(absolutePath) : absolutePath
+    })
+  )
 
-  return files.flat();
+  return files.flat()
 }
 
-rreaddir(__dirname + "/../templates/simple").then(result => console.log(result));
-
+rreaddir(`${__dirname}/../templates/nodejs-script`)
+  .then(result => console.log(result))
